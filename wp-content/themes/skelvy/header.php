5b0b8ab7352a80a66581af777795f6a7849c6f36
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-03-30 12:50:49 +0100

    feature(app) add google analytics

--- wp-content/themes/skelvy/header.php
+++ wp-content/themes/skelvy/header.php
@@ -4,6 +4,14 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-137345573-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-137345573-1');
+    </script>
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
