976543ac80715af7551889b32e1a5b5527b8e2fb
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-04-13 17:50:43 +0200

    bugfix(app) replace font logo with svg

--- wp-content/themes/skelvy/template-parts/navigation/navigation.php
+++ wp-content/themes/skelvy/template-parts/navigation/navigation.php
@@ -1,7 +1,7 @@
 <nav id="nav" class="navbar navbar-expand-lg speed-3">
   <div class="container">
     <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
-      <div class="logo">skelvy</div>
+      <img src="<?php bloginfo( 'template_url' ); ?>/template-parts/navigation/brand/text_white.svg" alt="logo" class="logo" />
     </a>
     <button class="navbar-toggler pointer"
             type="button"
