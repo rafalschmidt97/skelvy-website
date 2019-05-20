da40aa7dac8344a7bf9f612d9693d0ae7ed42fc5
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-05-20 12:51:02 +0200

    build(app) publish 1.0.0 release

--- wp-content/themes/skelvy/views/front-page/front-page.php
+++ wp-content/themes/skelvy/views/front-page/front-page.php
@@ -1,28 +1,28 @@
 <main class="front-page">
   <header class="header text-white d-flex">
-    <div class="my-auto w-100 py-5 py-lg-0">
-      <div class="container">
-        <div class="row no-gutters">
-          <div class="col-lg-7 d-lg-flex">
-            <?php
-            $current_language = '';
-
-            switch (get_locale()) {
-              case 'pl_PL':
-                $current_language = "mockup-pl";
-                break;
-              default:
-                $current_language = "mockup";
-            }
-            ?>
-            <img src="<?php bloginfo( 'template_url' ); ?>/views/front-page/images/<?php echo $current_language ?>.png" class="mockup my-auto img-fluid">
+    <div class="container">
+      <div class="row h-100 no-gutters">
+        <div class="col-xl-5 col-lg-4 d-flex text-center text-lg-left py-5 py-lg-0">
+          <div class="my-auto w-100">
+            <h1 class="header-text display-4 mb-4"><?php _e('Mobile app for meetings over your favorite drinks', 'skelvy'); ?></h1>
+            <a href="https://play.google.com/store/apps/details?id=com.skelvy" target="_blank" class="btn btn-lg btn-outline-light mr-2 mb-lg-2 mb-0">Google Play <i class="fab fa-google-play"></i></a>
+            <a href="https://itunes.apple.com/us/app/facebook/id1462518070" target="_blank" class="btn btn-lg btn-outline-light">App Store <i class="fab fa-app-store"></i></a>
           </div>
-          <div class="col-lg-5 d-flex text-center text-lg-left">
-            <div class="my-auto w-100">
-              <h1 class="header-text display-4 mb-4"><?php _e('Mobile app for meetings over your favorite drinks', 'skelvy'); ?></h1>
-              <a href="https://play.google.com/store/apps/details?id=com.skelvy" target="_blank" class="btn btn-lg btn-outline-light mr-2">Google Play <i class="fab fa-google-play"></i></a>
-              <a href="https://itunes.apple.com/us/app/facebook/id1462518070" target="_blank" class="btn btn-lg btn-outline-light">App Store <i class="fab fa-app-store"></i></a>
-            </div>
+        </div>
+        <?php
+          $current_language = '';
+
+          switch (get_locale()) {
+            case 'pl_PL':
+              $current_language = "mockup-pl";
+              break;
+            default:
+              $current_language = "mockup";
+          }
+        ?>
+        <div class="col-xl-7 col-lg-8 d-flex">
+          <div class="mt-auto">
+            <img class="mockup img-fluid" src="<?php bloginfo( 'template_url' ); ?>/views/front-page/images/<?php echo $current_language ?>.png'">
           </div>
         </div>
       </div>
