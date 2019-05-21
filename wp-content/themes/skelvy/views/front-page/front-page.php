97ed168c45a10de4338a7c19f2f39e57d5ce7f4d
Rafał Schmidt <rafalschmidt97@gmail.com>
2019-05-21 22:30:36 +0200

    bugfix(views) change url for app store

--- wp-content/themes/skelvy/views/front-page/front-page.php
+++ wp-content/themes/skelvy/views/front-page/front-page.php
@@ -6,7 +6,7 @@
           <div class="my-auto w-100">
             <h1 class="header-text display-4 mb-4"><?php _e('Mobile app for meetings over your favorite drinks', 'skelvy'); ?></h1>
             <a href="https://play.google.com/store/apps/details?id=com.skelvy" target="_blank" class="btn btn-lg btn-outline-light mr-2 mb-lg-2 mb-0">Google Play <i class="fab fa-google-play"></i></a>
-            <a href="https://itunes.apple.com/us/app/facebook/id1462518070" target="_blank" class="btn btn-lg btn-outline-light">App Store <i class="fab fa-app-store"></i></a>
+            <a href="https://itunes.apple.com/pl/app/skelvy/id1462518070" target="_blank" class="btn btn-lg btn-outline-light">App Store <i class="fab fa-app-store"></i></a>
           </div>
         </div>
         <?php
